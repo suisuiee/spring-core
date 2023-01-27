36e5e699849350d511825bf016d7a0661a245210
조수빈 <sbcho@icraft21.com>
2023-01-27 17:42:40 +0900

    스프링의 핵심 원리 이해 강의 수강 실습 코드 (@Configuration과 싱글톤)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,21 +15,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
     // memberService
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     // memberRepository
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     // orderService
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
